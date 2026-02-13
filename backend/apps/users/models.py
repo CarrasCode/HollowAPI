9821d881274423573e7bcd94f7c126d9f962c580
CarrasCode <antgranada@gmail.com>
2026-02-13 22:19:20 +0100

    feat: Add custom user

--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -1,14 +1,14 @@
-from typing import Any, override
+from typing import override
 
-from django.contrib.auth.base_user import BaseUserManager
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 
 # Create your models here.
 
 
 class CustomUserManager(BaseUserManager["CustomUser"]):
-    def create_user(self, email: str, password: str | None = None, **extra_fields: Any):
+    def create_user(self, email: str, password: str | None = None, **extra_fields):
         if not email:
             raise ValueError("El email es obligatorio")
         email = self.normalize_email(email)
@@ -17,21 +17,21 @@ class CustomUserManager(BaseUserManager["CustomUser"]):
         user.save(using=self.db)
         return user
 
-    def create_superuser(self, email: str, password: str | None = None, **extra_fields: Any):
+    def create_superuser(self, email: str, password: str | None = None, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
         return self.create_user(email, password, **extra_fields)
 
 
-class CustomUser(AbstractUser):
+class CustomUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    is_active = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-    register_at = models.TimeField(editable=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
 
-    #
+    objects: CustomUserManager = CustomUserManager()
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["first_name", "last_name"]
